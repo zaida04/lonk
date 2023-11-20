2bc0b7e60cb89377bd498f635e1321045b41cd11
Nico <nico.03727+github@gmail.com>
2023-11-20 06:10:55 +0000

    fix: pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,5 @@
 module.exports = {
     name: "lonk", // Name of your application
-    script: "src/index.ts", // Entry point of your application
-    interpreter: "~/.bun/bin/bun", // Path to the Bun interpreter
-  };
-  
+    script: "bun",
+    args: "run start",
+};
